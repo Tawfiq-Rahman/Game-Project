4cd823465f78c303083c37f64c3afd3fe04c7262
USER <rahmantawfiq007@gmail.com>
2024-02-19 02:46:08 +0600

    LearningSytemFirstPage

---
+++ src/main/java/com/example/project01/SchoolController.java
@@ -0,0 +1,49 @@
+package com.example.project01;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SchoolController {
+
+    @FXML
+    void Click_English(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_Geography(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_History(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_Mathematics(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_QEnglish(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_QGeography(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_QHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_QMathematics(ActionEvent event) {
+
+    }
+
+}
+
